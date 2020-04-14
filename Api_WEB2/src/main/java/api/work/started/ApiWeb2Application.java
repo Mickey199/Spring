26c3bec9b2ca703b59834b0cc8e404462a7b4b8f
mamejia <mamejia@STFMXQROL0167>
2020-04-13 22:02:58 -0500

    Comit inicial con eclipse

---
+++ Api_WEB2/src/main/java/api/work/started/ApiWeb2Application.java
@@ -0,0 +1,13 @@
+package api.work.started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWeb2Application.class, args);
+	}
+
+}
---
+++ Api_WEB2/src/test/java/api/work/started/ApiWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.work.started;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
